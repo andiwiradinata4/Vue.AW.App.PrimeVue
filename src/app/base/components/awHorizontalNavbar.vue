8b45090aca19f8488848bc0a7a9892bebba1096f
ASUS LAPTOP SPACE EDITION <andiwiradinata4@gmail.com>
2025-01-29 21:16:45 +0700

    add PanelMenu

--- src/app/base/components/awHorizontalNavbar.vue
+++ src/app/base/components/awHorizontalNavbar.vue
@@ -1,20 +1,33 @@
 <template>
     <div class="horizontal-navbar">
-        <Drawer v-model:visible="visible" header="Drawer">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.</p>
+        <Drawer v-model:visible="visible" header="ERPS" class="!w-full md:!w-80 lg:!w-[30rem]">
+            <awPanelMenu :fieldElement="fieldMenuItem" />
         </Drawer>
         <Button icon="pi pi-bars" @click="visible = true" />
         <h1 class="ml-4 font-semibold text-xl">ERPS</h1>
     </div>
 </template>
 
-<script setup>
+<script lang="ts">
 import Button from 'primevue/button';
 import Drawer from 'primevue/drawer';
-import { ref } from 'vue';
-const visible = ref(false);
+import awPanelMenu from './awPanelMenu.vue';
+import { defineComponent, ref } from 'vue';
+import { FieldElementItem } from '../models/fieldelementitem';
+export default defineComponent({
+    name: 'awHorizontalNavbar',
+    components: { Button, Drawer, awPanelMenu },
+    setup(props, { emit }: any) {
+        const visible = ref(false);
+        const fieldMenuItem = new FieldElementItem({
+
+        })
+
+        return { visible, fieldMenuItem };
+    },
+
+});
+
 </script>
 
 <style scoped>
---
+++ src/app/base/components/awPanelMenu.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="card flex justify-left">
+        <PanelMenu :model="items" multiple class="w-full">
+            <template #item="{ item }">
+                <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+                    <a class="flex items-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2"
+                        :href="href" @click="navigate">
+                        <span :class="item.icon" />
+                        <span class="ml-2">{{ item.label }}</span>
+                    </a>
+                </router-link>
+                <a v-else class="flex items-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2"
+                    :href="item.url" :target="item.target">
+                    <span :class="item.icon" />
+                    <span class="ml-2">{{ item.label }}</span>
+                    <span v-if="item.items" class="pi pi-angle-down text-primary ml-auto" />
+                </a>
+            </template>
+        </PanelMenu>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import PanelMenu from 'primevue/panelmenu';
+import Skeleton from 'primevue/skeleton';
+import { FieldElementItem } from '../models/fieldelementitem';
+export default defineComponent({
+    name: 'awPanelMenu',
+    components: { PanelMenu, Skeleton },
+    props: {
+        fieldElement: {
+            type: FieldElementItem,
+            required: true
+        }
+    },
+
+    emits: ['update:modelValue'],
+
+    setup(props, { emit }: any) {
+        const items = ref([{
+            label: 'Files',
+            icon: 'pi pi-file',
+            items: [
+                {
+                    label: 'Documents',
+                    icon: 'pi pi-file',
+                    items: [
+                        {
+                            label: 'Invoices',
+                            icon: 'pi pi-file-pdf',
+                            items: [
+                                {
+                                    label: 'Pending',
+                                    icon: 'pi pi-stop',
+                                    items: [
+                                        {
+                                            label: 'Paid',
+                                            icon: 'pi pi-check-circle'
+                                        }
+                                    ]
+
+                                },
+                                {
+                                    label: 'Paid',
+                                    icon: 'pi pi-check-circle'
+                                }
+                            ]
+                        },
+                        {
+                            label: 'Clients',
+                            icon: 'pi pi-users'
+                        }
+                    ]
+                },
+                {
+                    label: 'Images',
+                    icon: 'pi pi-image',
+                    items: [
+                        {
+                            label: 'Logos',
+                            icon: 'pi pi-image'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Cloud',
+            icon: 'pi pi-cloud',
+            items: [
+                {
+                    label: 'Upload',
+                    icon: 'pi pi-cloud-upload',
+                    route: '/theming/styled'
+                },
+                {
+                    label: 'Download',
+                    icon: 'pi pi-cloud-download'
+                },
+                {
+                    label: 'Sync',
+                    icon: 'pi pi-refresh'
+                }
+            ]
+        },
+        {
+            label: 'Devices',
+            icon: 'pi pi-desktop',
+            items: [
+                {
+                    label: 'Phone',
+                    icon: 'pi pi-mobile'
+                },
+                {
+                    label: 'Desktop',
+                    icon: 'pi pi-desktop'
+                },
+                {
+                    label: 'Tablet',
+                    icon: 'pi pi-tablet'
+                }
+            ]
+        }]);
+
+        return { items };
+    },
+});
+</script>
+
+<style scoped>
+/* Tambahkan gaya khusus jika diperlukan */
+</style>
